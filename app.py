a26b9585a4ed0f0b4a53e756daad7ac44f4faa64
danielj0nes <daniel@jones.ac>
2022-01-03 03:04:54 +0000

    Cleaning code; handling obvious errors

--- app.py
+++ app.py
@@ -16,32 +16,35 @@ def home():
 
 @app.route("/result", methods=["post"])
 def result():
-    # First obtain guaranteed form data
+    # First process certain form data and create timestamp
     comment_label = request.form["comment_label"]
     comment_language = request.form["language"]
-    # Check if a specific project path is specified; redundant feature?
-    if "project_path" in request.form:
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    # Check if a valid project path is specified; redundant feature?
+    if "project_path" in request.form and request.form["project_path"].strip() != "":
         project_path = request.form["project_path"]
         repo_name = project_path.split("/")[-1]
-    # Otherwise clone the repo
+    # Check if valid git URL provided
+    elif "git_url" in request.form and request.form["git_url"].strip() != "":
+        repo_name = request.form["git_url"].split("/")[-1]
+        project_path = f"projects/{repo_name}_{timestamp}"
+        Repo.clone_from(request.form["git_url"], project_path)
     else:
-        timestamp = datetime.now()
-        timestamp = timestamp.strftime("%Y%m%d%H%M%S")
-        project_path = f"projects/temp{timestamp}"
-        repo = Repo.clone_from(request.form["git_url"], project_path)
-        repo_name = repo.remotes.origin.url.split(".git")[0].split("/")[-1]
-    # Run the main.py script to generate the output as a json file - has to be this way on Ubuntu server
+        flash("Error: No git URL or project path specified.")
+        return render_template("index.html")
+    # Run the main.py script to generate the output as a json file
     try:
-        subprocess.run(f"python3 quality_assessment/src/main.py {project_path} outputs/{repo_name}_{timestamp}.json models/ --label {comment_label} --language {comment_language}",
-        shell=True)
+        # Using .split() solves the bug where the Python shell is opened if we pass the function a list
+        subprocess.run(f"python3 quality_assessment/src/main.py {project_path} outputs/{repo_name}_{timestamp}.json models/ --label {comment_label} --language {comment_language}".split())
     except subprocess.CalledProcessError:
         flash("An error occurred. Try checking your project path.")
     # Delete the cloned repo once finished
     rmtree(project_path)
+    # Load the JSON output and send it to the results page
     json_output = json.load(open(f"outputs/{repo_name}_{timestamp}.json"))
     return render_template(
         "results.html",
-        response=json.dumps(json_output, sort_keys=True, indent=2, separators=(',', ': '))
+        response=json.dumps(json_output, indent=2)
         )
 
 if __name__ == "__main__":
